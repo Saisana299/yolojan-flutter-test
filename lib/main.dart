3ddd332b61d503f85c80a6f2c666f549b8b39c38
Saisana299 <yt0521kariaka@gmail.com>
2024-12-24 19:52:57 +0900

    [Edit]: flutter_vision

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:camera/camera.dart';
 import 'package:custom_ratio_camera/custom_ratio_camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_vision/flutter_vision.dart';
 import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:yolojan/theme.dart';
 
@@ -12,6 +13,7 @@ late List<CameraDescription> _cameras;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   _cameras = await availableCameras();
+
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
@@ -43,27 +45,56 @@ class CameraPage extends StatefulWidget {
 class CameraPageState extends State<CameraPage> {
   late CameraController controller;
   late Future<void> initializeControllerFuture;
+  late FlutterVision vision;
+  late List<Map<String, dynamic>> yoloResults;
+
+  CameraImage? cameraImage;
+  bool isLoaded = false;
+  bool isDetecting = false;
+
+  Duration throttleDuration = const Duration(milliseconds: 100);
+  DateTime? lastProcessedTime;
 
   @override
   void initState() {
     super.initState();
 
+    vision = FlutterVision();
+
     controller = CameraController(
       _cameras[0],
       ResolutionPreset.max,
+      enableAudio: false,
     );
 
-    initializeControllerFuture = controller.initialize();
+    initializeControllerFuture = controller.initialize().then((value) {
+      loadYoloModel().then((value) {
+        setState(() {
+          isLoaded = true;
+          isDetecting = false;
+          yoloResults = [];
+        });
+      });
+    });
   }
 
   @override
-  void dispose() {
+  void dispose() async {
     controller.dispose();
     super.dispose();
+    await vision.closeYoloModel();
   }
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    if (!isLoaded) {
+      return const Scaffold(
+        body: Center(
+          child: Text("Model not loaded, waiting for it"),
+        ),
+      );
+    }
     return Scaffold(
       body: Container(
         color: Colors.black,
@@ -72,12 +103,17 @@ class CameraPageState extends State<CameraPage> {
             future: initializeControllerFuture,
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
-                return AspectRatio(
-                  aspectRatio: 16/9,
-                  child: CustomRatioCameraPreview(
-                    cameraController: controller,
-                    expectedRatio: 16/9
-                  )
+                return Stack(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 16/9,
+                      child: CustomRatioCameraPreview(
+                        cameraController: controller,
+                        expectedRatio: 16/9,
+                      ),
+                    ),
+                    ...displayBoxesAroundRecognizedObjects(size),
+                  ],
                 );
               } else {
                 return const CircularProgressIndicator();
@@ -87,20 +123,126 @@ class CameraPageState extends State<CameraPage> {
         ),
       ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final image = await controller.takePicture();
-          if (!context.mounted) return;
-          await Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => DisplayPictureScreen(image: image),
-              fullscreenDialog: true,
-            )
-          );
-        },
-        child: const Icon(Icons.camera_alt),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () async {
+              final image = await controller.takePicture();
+              if (!context.mounted) return;
+              await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DisplayPictureScreen(image: image),
+                  fullscreenDialog: true,
+                )
+              );
+            },
+            child: const Icon(Icons.camera_alt),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: () {
+              if(isDetecting) {
+                stopDetection();
+              } else {
+                startDetection();
+              }
+            },
+            child: Icon(isDetecting ? Icons.stop : Icons.play_arrow),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> loadYoloModel() async {
+    await vision.loadYoloModel(
+      labels: 'assets/labels.txt',
+      modelPath: 'assets/yolov8n.tflite',
+      modelVersion: "yolov8",
+      numThreads: 8,
+      useGpu: true,
+      quantization: false,
+    );
+    setState(() {
+      isLoaded = true;
+    });
+  }
+
+  Future<void> yoloOnFrame(CameraImage cameraImage) async {
+    final result = await vision.yoloOnFrame(
+      bytesList: cameraImage.planes.map((plane) => plane.bytes).toList(),
+      imageHeight: cameraImage.height,
+      imageWidth: cameraImage.width,
+      iouThreshold: 0.4,
+      // confThreshold: 0.5,
+      classThreshold: 0.5,
+    );
+    if(result.isNotEmpty) {
+      setState(() {
+        yoloResults = result;
+      });
+    }
+  }
+
+  Future<void> startDetection() async {
+    setState(() {
+      isDetecting = true;
+    });
+    if(controller.value.isStreamingImages) {
+      return;
+    }
+    await controller.startImageStream((image) async {
+      if (isDetecting) {
+        // final currentTime = DateTime.now();
+        // if (
+        //   lastProcessedTime == null ||
+        //   currentTime.difference(lastProcessedTime!) > throttleDuration
+        // ) {
+        //   lastProcessedTime = currentTime;
+          cameraImage = image;
+          yoloOnFrame(image);
+        // }
+      }
+    });
+  }
+
+  Future<void> stopDetection() async {
+    setState(() {
+      isDetecting = false;
+      yoloResults.clear();
+    });
+  }
+
+  List<Widget> displayBoxesAroundRecognizedObjects(Size screen) {
+    if (yoloResults.isEmpty) return [];
+    double factorX = screen.width / (cameraImage?.height ?? 1);
+    double factorY = screen.height / (cameraImage?.width ?? 1);
+
+    Color colorPick = const Color.fromARGB(255, 50, 233, 30);
+
+    return yoloResults.map((result) {
+      return Positioned(
+        left: result["box"][0] * factorX,
+        top: result["box"][1] * factorY,
+        width: (result["box"][2] - result["box"][0]) * factorX,
+        height: (result["box"][3] - result["box"][1]) * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            border: Border.all(color: Colors.pink, width: 2.0),
+          ),
+          child: Text(
+            "${result['tag']} ${(result['box'][4] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = colorPick,
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
   }
 }
 
@@ -125,7 +267,7 @@ class DisplayPictureScreen extends StatelessWidget {
             Navigator.of(context).pop();
           }
         },
-        child: const Icon(Icons.save_alt),
+        child: const Icon(Icons.save),
       ),
     );
   }
